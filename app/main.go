2c1996418e6c772115e4986128f2454114e9bf29
ricsae <eduricsa@hotmail.com>
2025-07-30 11:21:57 -0600

    test2 stage 2

--- app/main.go
+++ app/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
@@ -11,23 +13,33 @@ var _ = net.Listen
 var _ = os.Exit
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-// ``	Uncomment this block to pass the first stage
 	
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
+
 	for {
 		conn, err := l.Accept()
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
+		go handleConnection(conn)
 	
-		conn.Write([]byte("+PONG\r\n"))
 	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		text:= scanner.Text()
+		if strings.TrimSpace(text) == "PING" {
+			conn.Write([]byte("PONG\r\n"))
+		}
+	}
+
 }
