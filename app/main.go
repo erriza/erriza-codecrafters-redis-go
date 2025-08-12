8d036ad1bddcde52415ba1762674a83d9d68c486
ricsae <eduricsa@hotmail.com>
2025-08-12 12:45:53 -0600

    Echo command test1

--- app/main.go
+++ app/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -33,13 +35,75 @@ func main() {
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
+	reader := bufio.NewReader(conn)
 
-	scanner := bufio.NewScanner(conn)
-	for scanner.Scan() {
-		text := scanner.Text()
-		if strings.TrimSpace(text) == "PING" {
+	for {
+		// Read array header (*N\r\n)
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			return // client disconnected or error
+		}
+
+		line = strings.TrimSuffix(line, "\r\n")
+		if len(line) == 0 || line[0] != '*' {
+			return // protocol error or close
+		}
+
+		n, err := strconv.Atoi(line[1:])
+		if err != nil {
+			return
+		}
+
+		args := make([]string, 0, n)
+
+		for i := 0; i < n; i++ {
+			bulkLenLine, err := reader.ReadString('\n')
+			if err != nil {
+				return
+			}
+
+			bulkLenLine = strings.TrimSuffix(bulkLenLine, "\r\n")
+			if len(bulkLenLine) == 0 || bulkLenLine[0] != '$' {
+				return
+			}
+
+			bulkLen, err := strconv.Atoi(bulkLenLine[1:])
+			if err != nil || bulkLen < 0 {
+				return
+			}
+
+			bulk := make([]byte, bulkLen)
+			_, err = io.ReadFull(reader, bulk)
+			if err != nil {
+				return
+			}
+
+			if _, err := reader.Discard(2); err != nil {
+				return
+			}
+
+			args = append(args, string(bulk))
+		}
+
+		if len(args) == 0 {
+			continue
+		}
+
+		cmd := strings.ToUpper(args[0])
+
+		switch cmd {
+		case "PING":
 			conn.Write([]byte("+PONG\r\n"))
+		case "ECHO":
+			if len(args) >= 2 {
+				msg := args[1]
+				resp := fmt.Sprintf("$%d\r\n%s\r\n", len(msg), msg)
+				conn.Write([]byte(resp))
+			} else {
+				conn.Write([]byte("$0\r\n\r\n"))
+			}
+		default:
+			conn.Write([]byte("-ERR unknown command\r\n"))
 		}
 	}
-
 }
